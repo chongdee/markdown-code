b1170435bcda5777b74052451f603f8627d40515
yuyongxing <yyxpublic@163.com>
2022-03-01 14:01:05 +0800

    埋点sdk

--- 埋点jssdk/data.js
+++ 埋点jssdk/data.js
@@ -2,32 +2,39 @@
  * @Author: yuyongxing
  * @Date: 2022-02-28 10:22:43
  * @LastEditors: yuyongxing
- * @LastEditTime: 2022-02-28 18:10:34
+ * @LastEditTime: 2022-03-01 13:59:11
  * @Description: 用户信息采集
  */
 (function () {
     var _track = {
-        baseUrl:"https://hmma.baidu.com/mini.gif",
+        baseUrl: "https://hmma.baidu.com/mini.gif",
         v: "1.0.0",
-        debug:false,
+        debug: false,
         t: {
             u: window.location.host,
             et: Date.now() + 300,
             ua: getUA()
         },
-        track:function(params={}){
-            let data = Object.assign({t:this.t},params)
-           let img = new Image()
-           img.onerror = function(){}
-           img.src = getSendUrl(this.baseUrl,data)
+        track: function (params = {}) {
+            let data = Object.assign({ t: this.t }, params)
+            let img = new Image()
+            img.onerror = function () {
+                //失败时使用其他方式重试 
+            }
+            img.src = getSendUrl(this.baseUrl, data)
         },
         init: function (options) {
-            this.debug = isBoolean(options.debug) && options.debug ||false
-            
+            this.debug = isBoolean(options.debug) && options.debug || false
+            this.track()
         }
     }
-    function getSendUrl(){
-        
+    function getSendUrl(url,params) {
+        console.log(params,"par")
+        let paramsStr = ""
+        for(var i in params){
+            paramsStr+= i+"="+i
+        }
+          return url+'?'+paramsStr
     }
     function getUA() {
         var Sys = {};
@@ -52,6 +59,6 @@
     }
     function isBoolean(obj) {
         return toString.call(obj) == '[object Boolean]';
-      }
+    }
     window._track = _track
 })()
